f519e34dea9828457089387bb5d16c19b8f05a54
Pavel Kabir <pkabir@acumatica.com>
2016-08-24 14:24:50 +0300

    Restore more necessary packages

--- defineconstants.ps1
+++ defineconstants.ps1
@@ -1,9 +1,12 @@
 ﻿# Some constants that you might wish to redefine
 # - 
 $global:repositoryPath = "c:\repos\code\"
+
 $global:webConfigDirectory = $repositoryPath + "WebSites\Pure\Site\"
 $global:webConfigPath = $webConfigDirectory + "Web.config"
 
 $global:pureSolutionPath = "WebSites\Pure\Pure.sln"
 $global:netToolsSolutionPath = "NetTools\NetTools.sln"
 $global:pureWithNetToolsSolutionPath = "PureWithNetTools.sln"
+
+$global:sitePackagesConfigPath = $repositoryPath + "WebSites\Pure\Site\packages.config"
--- restorenugetpackages.ps1
+++ restorenugetpackages.ps1
@@ -6,5 +6,6 @@ cd $repositoryPath
 
 nuget restore $netToolsSolutionPath -NonInteractive -NoCache -ConfigFile ($repositoryPath + "NuGet.config")
 nuget restore $pureSolutionPath -NonInteractive -NoCache -ConfigFile ($repositoryPath + "NuGet.config")
+nuget restore $sitePackagesConfigPath -NonInteractive -NoCache -ConfigFile ($repositoryPath + "NuGet.config")
 
 Write-Host "NuGet packages restored" -ForegroundColor Green
